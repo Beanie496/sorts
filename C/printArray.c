d5096dfe9b078790428d39a20e1c6bee294c78de
Jasper Shovelton <Beanie.github@proton.me>
2023-04-05 21:42:22 +0100

    Display the array length after printing it

--- C/printArray.c
+++ C/printArray.c
@@ -8,5 +8,5 @@ void printArray(int *array, int length)
 	printf("%d", array[0]);
 	for (int i = 1; i < length; i++)
 		printf(", %d", array[i]);
-	printf("\n");
+	printf("\nLength: %d\n", length);
 }
