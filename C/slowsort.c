282da12452a86564f1aa153c0d5927f39ea7add2
Jasper Shovelton <Beanie.github@proton.me>
2023-04-05 05:02:36 +0100

    update slowsort

--- C/slowsort.c
+++ C/slowsort.c
@@ -1,62 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "sorts.h"
 
-
-void swap(int* start, int* end)
-{
-    int temp = *end;
-    *end = *start;
-    *start = temp;
-}
+static void swap(int* start, int* end);
 
 
 void slowsort(int array[], int length)
 {
-    if (length > 2) {
-        
-        slowsort(array, length/2);
-        slowsort(array + length/2, (length + 1)/2);
+	if (length == 1)
+		return;
 
-        if (array[length/2 - 1] > array[length - 1])
-            swap(array + length/2 - 1, array + length - 1);
+	slowsort(array, length / 2);
+	slowsort(array + length / 2, (length + 1) / 2);
 
-        slowsort(array, length - 1);
+	if (array[length / 2 - 1] > array[length - 1])
+		swap(array + length / 2 - 1, array + length - 1);
 
-    } else 
-        if (array[0] > array[1])
-            swap(array, array + 1);
-         
+	slowsort(array, length - 1);
 }
 
 
-int main()
+void swap(int* start, int* end)
 {
-    int arrayLength = 300;
-    int arrayRange = 100;
-    int array[arrayLength];
-    int i;
-    
-    srand(time(NULL));
-
-
-    array[0] = rand() % (arrayRange + 1);
-    printf("%d", array[0]);
-    for (i = 0; i < arrayLength; i++) {
-        array[i] = rand() % (arrayRange + 1);
-        printf(", %d", array[i]);
-    }
-    printf("\nSorting...\n");
-
-
-    slowsort(array, arrayLength);
-
-
-    printf("\nSorted array:\n");
-    printf("%d", array[0]);
-    for (i = 0; i < arrayLength; i++)
-        printf(", %d", array[i]);
-    printf("\n[arrayLength = %d]\n", arrayLength);
-
-    return 0;
+	int temp = *end;
+	*end = *start;
+	*start = temp;
 }
