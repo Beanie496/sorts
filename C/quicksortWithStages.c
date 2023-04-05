cd34e045d2aeb2d5073634f9944d306e6259ab75
Jasper Shovelton <Beanie.github@proton.me>
2023-04-05 04:53:42 +0100

    update quicksort that has shown stages

---
+++ C/quicksortWithStages.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#include "sorts.h"
+
+static void partition(int *start, int* end, int *iteration);
+static void swap(int *start, int* end);
+
+
+void quicksortWithStages(int array[], int length)
+{
+	int iteration = 1;
+	partition(array, array + length - 1, &iteration);
+}
+
+
+void partition(int *start, int* end, int *iteration)
+{
+	if (start > end - 1)
+		return;
+
+	printf("Pass %d: ", (*iteration)++);
+	printArray(start, end - start + 1);
+
+	int *pivot = end;
+	int *p1 = start;
+	int *p2 = p1 - 1;
+
+	// this partitions the array
+	while (++p2 < pivot)
+		if (*p2 < *pivot)
+			swap(p1++, p2);
+	
+	swap(p1, pivot);
+
+	partition(start, p1 - 1, iteration);
+	partition(p1 + 1, end, iteration);
+}
+
+
+void swap(int *start, int* end)
+{
+	int temp = *end;
+	*end = *start;
+	*start = temp;
+}
