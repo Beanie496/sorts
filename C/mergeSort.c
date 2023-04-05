71d59fdefd7360390cf8c91d4366fc73f74cb15d
Jasper Shovelton <Beanie.github@proton.me>
2023-04-05 04:00:01 +0100

    update merge sort

---
+++ C/mergeSort.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+
+#include "sorts.h"
+
+
+void mergeSort(int array[], int length)
+{
+	if (length == 1)
+		return;
+
+	int halfOfLength = length >> 1;
+
+	mergeSort(array, halfOfLength);
+	mergeSort(array + halfOfLength, length + 1 >> 1);
+
+	merge(array, array + halfOfLength, length);
+}
+
+
+void merge(int *array1, int *array2, int length)
+{
+	int *secondaryArray = malloc(length * sizeof(int));
+
+	int *mainArray = array1;
+	int *array1End = array2 - 1;
+	int *array2End = array1 + length - 1;
+
+	int i = 0;
+
+
+	while (array1 <= array1End && array2 <= array2End) {
+		if (*array2 < *array1)
+			secondaryArray[i] = *array2++;
+		else
+			secondaryArray[i] = *array1++;
+		i++;
+	}
+
+	// If the second list's start pointer has gone past its end, that means that it's gone through all its elements. That means that list 1 still has elements in it.
+	if (array2 > array2End)
+		while (array1 <= array1End)
+			secondaryArray[i++] = *array1++;
+	// And vice versa.
+	else
+		while (array2 <= array2End)
+			secondaryArray[i++] = *array2++;
+
+	for (i = 0; i < length; i++)
+		mainArray[i] = secondaryArray[i];
+
+
+	free(secondaryArray);
+}
