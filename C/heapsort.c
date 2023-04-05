db3afe671ca495868de3c07f083b3c191d5d7697
Jasper Shovelton <Beanie.github@proton.me>
2023-04-05 03:21:30 +0100

    update heapsort

--- C/heapsort.c
+++ C/heapsort.c
@@ -1,137 +1,86 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "sorts.h"
+
+static void siftDown(int array[], int *startPoint, int length);
+static void heapify(int array[], int length);
+static void swap(int *p1, int *p2);
 
 
-void sift_down(int array[], int* startPosPoint, int length)
+void heapsort(int array[], int length)
 {
-    int pos = startPosPoint - array;
-    int curEl; // Current element
-    int posOfFirstChild = (pos << 1) + 1;
+	int temp;
+	int *end = array + length - 1;
 
 
-    /*
-     * This loop swaps the current element with the higher of
-     * its two children. If the current position in the binary
-     * tree is such that there aren't two children, this loop stops.
-     * After that, it checks if there was only one child and swaps
-     * with it if needed.
-     *
-     * Logic behind this logical statement:
-     * If there is only one child, the right-hand side will be
-     * equal to pos. If there are two, the right-hand side will
-     * be one less. This ensures the loop is broken as soon as
-     * there are fewer than two children.
-     */
+	heapify(array, length);
 
-    while (pos < length - 1 >> 1) {
+	while (array < end) {
 
-        curEl = array[pos];
-        
+		temp = *end;
+		*end = array[0];
+		array[0] = temp;
 
-        if (array[pos] < array[posOfFirstChild] || array[pos] < array[posOfFirstChild + 1]) {
-            if (array[posOfFirstChild + 1] > array[posOfFirstChild]) {
+		length--;
+		end--;
 
-                array[pos] = array[posOfFirstChild + 1];
-                array[posOfFirstChild + 1] = curEl;
-                pos = posOfFirstChild + 1;
+		siftDown(array, array, length);
 
-            } else {
-
-                array[pos] = array[posOfFirstChild];
-                array[posOfFirstChild] = curEl;
-                pos = posOfFirstChild;
-
-            }
-            
-
-            posOfFirstChild = (pos << 1) + 1;
-        } else
-            break;
-        
-    }
-
-
-    if (posOfFirstChild == length - 1) {
-
-        if (array[posOfFirstChild] > array[pos]) {
-
-            curEl = array[pos];
-            array[pos] = array[posOfFirstChild];
-            array[posOfFirstChild] = curEl;
-
-        }
-    }
-            
+	}
 }
 
 
 void heapify(int array[], int length)
 {
-    // I am decrementing it before I check, so I have to increment it by one beforehand. -1 + 1 = 0.
-    int* end = array + length/2;
+	int *end = array + (length >> 1);
 
-    while (end > array) {
-        end--;
-        sift_down(array, end, length);
-    }
+	while (end > array)
+		siftDown(array, --end, length);
 }
 
 
-void heapsort(int array[], int length)
+void siftDown(int array[], int *startPos, int length)
 {
-    int temp;
-    int* end = array + length - 1;
+	int pos = startPos - array;
+	int newPos;
+	int posOfFirstChild = (pos << 1) + 1;
 
-    heapify(array, length);
 
-    while (array < end) {
+	/*
+	 * This loop swaps the current element with the higher of
+	 * its two children. If the current position in the binary
+	 * tree is such that there aren't two children, this loop stops.
+	 * After that, it checks if there was only one child and swaps
+	 * with it if needed.
+	 *
+	 * Logic behind this logical statement:
+	 * If there is only one child, the right-hand side will be
+	 * equal to pos. If there are two, the right-hand side will
+	 * be one less. This ensures the loop is broken as soon as
+	 * there are fewer than two children.
+	 */
+	while (pos < length - 1 >> 1) {
+		if (!(array[pos] < array[posOfFirstChild] || array[pos] < array[posOfFirstChild + 1]))
+			break;
 
-        temp = *end;
-        *end = array[0];
-        array[0] = temp;
+		if (array[posOfFirstChild + 1] > array[posOfFirstChild])
+			newPos = posOfFirstChild + 1;
+		else
+			newPos = posOfFirstChild;
+		swap(&array[pos], &array[newPos]);
+		pos = newPos;
 
-        length--;
-        end--;
-        
-        sift_down(array, array, length);
+		posOfFirstChild = (pos << 1) + 1;
+	}
 
-    }
+
+	if (posOfFirstChild == length - 1)
+		if (array[posOfFirstChild] > array[pos])
+			swap(&array[pos], &array[posOfFirstChild]);
 }
 
 
-int main ()
+void swap(int *p1, int *p2)
 {
-    int arrayLength = 10000000; // set these
-    int arrayRange = 1000;      // to whatever 
-    int* array = malloc(arrayLength*4);
-    int i;
-
-    srand(time(0));
-    
-    
-    array[0] = rand() % (arrayRange + 1);
-    printf("%d", array[0]);
-    for (i = 1; i < arrayLength; i++)
-    {
-        array[i] = rand() % (arrayRange + 1);
-	    printf(", %d", array[i]);
-    }
-    printf("\nSorting...\n");
-
-    
-    heapsort(array, arrayLength);
-    
-     
-    printf("\nSorted array:\n");
-    printf("%d", array[0]);
-    for (i = 1; i < arrayLength; i++)
-    {
-   	    printf(", %d", array[i]);
-    }
-    printf("\n[arrayLength = %d]\n", arrayLength);
-
-    free(array);
-
-    return 0;
+	int temp = *p1;
+	*p1 = *p2;
+	*p2 = temp;
 }
