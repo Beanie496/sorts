b0faa74a1f199abcdabfbfa76ffe5c498b2c3e7b
Jasper Shovelton <Beanie.github@proton.me>
2023-08-02 11:12:59 +0100

    simplify

--- C/util.c
+++ C/util.c
@@ -8,13 +8,11 @@
 int *initArray(int length, int range)
 {
 	int *array = malloc(length * sizeof(int));
-	int i;
 
 	srand(time(0));
 
-	array[0] = rand() % (range + 1);
 	// initialising array with random numbers from 0 to 'range'
-	for (i = 1; i < length; i++)
+	for (int i = 0; i < length; i++)
 		array[i] = rand() % (range + 1);
 
 	return array;
