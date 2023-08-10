55a5294aef5bc9e9266b0c7a5f22b3917d6594af
Jasper Shovelton <Beanie.github@proton.me>
2023-08-10 03:04:24 +0100

    Cleanup
    - Rename 'maxNumber' to the less verbose 'max'
    - Reduce the scope of 'i' to each for loop that uses it
    - Delegate getting the max number to the function in util.c
    - Fix a spelling error
    - Use memmove() for shifting the digits of an array along instead of a for loop
    - Use memcpy() for copying one array to another instead of a for loop

--- C/countingSort.c
+++ C/countingSort.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "sorts.h"
 #include "util.h"
@@ -7,42 +8,36 @@
 void countingSort(int array[], int length)
 {
 	int *secondaryArray = malloc(length * sizeof(int));
-	int maxNumber = 0;
-	int i;
+	int max = getHighestNumberInArray(array, length);
+	// '+ 1' because it's 0 to max, not 1
+	int countingList[max + 1];
 
-	// This finds the highest number so it knows how long to make the counting list
-	for (i = 0; i < length; i++)
-		if (array[i] > maxNumber)
-			maxNumber = array[i];
-	int countingList[maxNumber + 1]; // '+ 1' because it's 0 to maxNumber, not 1
-
-	for (i = 0; i < maxNumber + 1; i++)
+	// can't initialise variable-sized object with 0's
+	for (int i = 0; i < max + 1; i++)
 		countingList[i] = 0;
 
 	// count occurances of each number
-	for (i = 0; i < length; i++)
+	for (int i = 0; i < length; i++)
 		countingList[array[i]]++;
 
-	// make each entry the cumalative sum of all the previous entries
-	// TODO: sp?
-	for (i = 1; i < maxNumber + 1; i++)
+	// make each entry the cumulative sum of all the previous entries
+	for (int i = 1; i < max + 1; i++)
 		countingList[i] += countingList[i - 1];
 
-	// shift each number along in order to get the starting index of each number
-	for (i = maxNumber; i >= 0; i--)
-		countingList[i + 1] = countingList[i];
+	// shift each number along in order to get the starting index of each
+	// number
+	memmove(&countingList[1], &countingList[0], (max + 1) * sizeof(*countingList));
 	countingList[0] = 0;
 
-	for (i = 0; i < length; i++) {
+	for (int i = 0; i < length; i++) {
 		// get the right index for the number and insert it
 		secondaryArray[countingList[array[i]]] = array[i];
-		// increment the index, as the next identical number needs to be stored
-		// in the next index
+		// increment the index, as the next identical number needs to
+		// be stored in the next index
 		countingList[array[i]]++;
 	}
 
-	for (i = 0; i < length; i++)
-		array[i] = secondaryArray[i];
+	memcpy(array, secondaryArray, sizeof(*array) * length);
 
 	free(secondaryArray);
 }
