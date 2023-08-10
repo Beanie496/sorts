76fcfe5ca695247d1a04e5d5596f8a157c51e97b
Jasper Shovelton <Beanie.github@proton.me>
2023-08-10 20:46:36 +0100

    Cast function pointers to avoid annoying warnings

--- C/main.c
+++ C/main.c
@@ -13,49 +13,49 @@ int main(int argc, char *argv[])
 	// TODO: command-line args
 
 	// 100,000,000
-	dosort(100000000, &binaryRadixSort, "Binary radix sort");
+	dosort(100000000, (int (*)(int *, int))binaryRadixSort, "Binary radix sort");
 
 	// 400,000
-	dosort(400000, &binaryInsertionSort, "Binary search insertion sort");
+	dosort(400000, (int (*)(int *, int))binaryInsertionSort, "Binary search insertion sort");
 
 	// 11
 	// you can try 12, but do you really want to wait that long?
 	// remember bogo sorts are O(n!)
-	int iterations = dosort(11, &bogoSort, "Bogo sort");
+	int iterations = dosort(11, bogoSort, "Bogo sort");
 	printf("Iterations: %d\n\n", iterations);
 
 	// 40,000
-	dosort(40000, &bubbleSort, "Bubble sort");
+	dosort(40000, (int (*)(int *, int))bubbleSort, "Bubble sort");
 
 	// 100,000,000
-	dosort(100000000, &countingSort, "Counting sort");
+	dosort(100000000, (int (*)(int *, int))countingSort, "Counting sort");
 
 	// 100,000,000
-	dosort(100000000, &decimalRadixSort, "Decimal radix sort");
+	dosort(100000000, (int (*)(int *, int))decimalRadixSort, "Decimal radix sort");
 
 	// 10,000,000
-	dosort(10000000, &heapsort, "Heapsort");
+	dosort(10000000, (int (*)(int *, int))heapsort, "Heapsort");
 
 	// 100,000,000
-	dosort(100000000, &hexidecimalRadixSort, "Hexidecimal radix sort");
+	dosort(100000000, (int (*)(int *, int))hexidecimalRadixSort, "Hexidecimal radix sort");
 
 	// 10,000,000
-	dosort(10000000, &mergeSort, "Merge sort");
+	dosort(10000000, (int (*)(int *, int))mergeSort, "Merge sort");
 
 	// 1,000,000
-	dosort(1000000, &quicksort, "Quicksort");
+	dosort(1000000, (int (*)(int *, int))quicksort, "Quicksort");
 
 	// 100
-	dosort(100, &quicksortWithStages, "Quicksort with stages");
+	dosort(100, (int (*)(int *, int))quicksortWithStages, "Quicksort with stages");
 
 	// 250
-	dosort(250, &slowsort, "Slowsort");
+	dosort(250, (int (*)(int *, int))slowsort, "Slowsort");
 
 	// 2,000
-	dosort(2000, &stoogeSort, "Stooge sort");
+	dosort(2000, (int (*)(int *, int))stoogeSort, "Stooge sort");
 
 	// 100,000,000
-	dosort(100000000, &threadedMergeSort, "Threaded merge sort");
+	dosort(400000000, (int (*)(int *, int))threadedMergeSort, "Threaded merge sort");
 
 	return 0;
 }
