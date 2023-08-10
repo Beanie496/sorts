b4ba72c20d1e7535b79da1740787245c16b062f7
Jasper Shovelton <Beanie.github@proton.me>
2023-08-10 18:37:43 +0100

    Cleanup
    - Remove temporary variable and use a division instead of a bit shift. The
    compiler is smart enough to do that for me (and even if it doesn't, it's losing
    mere nanoseconds).
    - Use memcpy() instead of manually doing it with a for loop

--- C/mergeSort.c
+++ C/mergeSort.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "sorts.h"
 #include "util.h"
@@ -11,46 +12,39 @@ void mergeSort(int array[], int length)
 	if (length == 1)
 		return;
 
-	int halfOfLength = length >> 1;
+	mergeSort(array, length / 2);
+	mergeSort(array + length / 2, (length + 1) / 2);
 
-	mergeSort(array, halfOfLength);
-	mergeSort(array + halfOfLength, length + 1 >> 1);
-
-	merge(array, array + halfOfLength, length);
+	merge(array, array + length / 2, length);
 }
 
 
 void merge(int *array1, int *array2, int length)
 {
 	int *secondaryArray = malloc(length * sizeof(int));
-
 	int *mainArray = array1;
 	int *array1End = array2 - 1;
 	int *array2End = array1 + length - 1;
+	int idx = 0;
 
-	int i = 0;
-
-
-	while (array1 <= array1End && array2 <= array2End) {
+	while (array1 <= array1End && array2 <= array2End)
 		if (*array2 < *array1)
-			secondaryArray[i] = *array2++;
+			secondaryArray[idx++] = *array2++;
 		else
-			secondaryArray[i] = *array1++;
-		i++;
-	}
+			secondaryArray[idx++] = *array1++;
 
-	// If the second list's start pointer has gone past its end, that means that it's gone through all its elements. That means that list 1 still has elements in it.
+	// If the second list's start pointer has gone past its end, that means
+	// that it's gone through all its elements. That means that list 1
+	// still has elements in it.
 	if (array2 > array2End)
 		while (array1 <= array1End)
-			secondaryArray[i++] = *array1++;
+			secondaryArray[idx++] = *array1++;
 	// And vice versa.
 	else
 		while (array2 <= array2End)
-			secondaryArray[i++] = *array2++;
+			secondaryArray[idx++] = *array2++;
 
-	for (i = 0; i < length; i++)
-		mainArray[i] = secondaryArray[i];
-
+	memcpy(mainArray, secondaryArray, sizeof(*mainArray) * length);
 
 	free(secondaryArray);
 }
