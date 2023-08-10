c3404dcfdcf6a409c2900d287ce5b2c0aaee38b1
Jasper Shovelton <Beanie.github@proton.me>
2023-08-10 03:16:59 +0100

    Cleanup. Almost same as previous commit.
    - Rename 'maxNumber' to 'max'
    - Reduce the scope of 'i'
    - Delegate getting the max number to the function in util.c
    - Use memmove() and memcpy() instead of for loops

--- C/decimalRadixSort.c
+++ C/decimalRadixSort.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "sorts.h"
 #include "util.h"
@@ -8,16 +9,9 @@ static void digitCountingSort(int array[], int length, int digit);
 
 void decimalRadixSort(int array[], int length)
 {
-	int maxNumber = 0;
-	int i;
+	int max = getHighestNumberInArray(array, length);
 
-	// This finds the highest number so it knows how many times to iterate
-	for (i = 0; i < length; i++)
-		if (array[i] > maxNumber)
-			maxNumber = array[i];
-
-
-	for (i = 1; i < maxNumber + 1; i *= 10)
+	for (int i = 1; i < max + 1; i *= 10)
 		digitCountingSort(array, length, i);
 }
 
@@ -26,25 +20,23 @@ void digitCountingSort(int array[], int length, int digit)
 {
 	int valuesTotal[10] = { 0 };
 	int *secondaryArray = malloc(length * sizeof(int));
-	int i;
 
 	// This counts how many there are of each of the LSDs of the numbers
-	for (i = 0; i < length; i++)
+	for (int i = 0; i < length; i++)
 		valuesTotal[(array[i] / digit) % 10]++;
 
-	for (i = 1; i < 10; i++)
+	for (int i = 1; i < 10; i++)
 		valuesTotal[i] += valuesTotal[i - 1];
 
-	for (i = 8; i >= 0; i--)
-		valuesTotal[i + 1] = valuesTotal[i];
+	memmove(&valuesTotal[1], &valuesTotal[0], 9 * sizeof(*valuesTotal));
 	valuesTotal[0] = 0;
 
-	for (i = 0; i < length; i++) {
+	for (int i = 0; i < length; i++) {
 		secondaryArray[valuesTotal[(array[i] / digit) % 10]] = array[i];
 		valuesTotal[(array[i] / digit) % 10]++;
 	}
 
-	for (i = 0; i < length; i++)
-		array[i] = secondaryArray[i];
+	memcpy(array, secondaryArray, sizeof(*array) * length);
+
 	free(secondaryArray);
 }
