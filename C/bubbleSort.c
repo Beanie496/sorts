adcecba94a49ee3b90bac3243ef8d70b270e6b4b
Jasper Shovelton <Beanie.github@proton.me>
2023-10-04 18:00:24 +0100

    Speed up bubble sort by ~100%

--- C/bubbleSort.c
+++ C/bubbleSort.c
@@ -6,9 +6,9 @@ void bubbleSort(int array[], int length)
 {
 	int sorted = 0;
 
-	while (!sorted) {
+	for (int j = length - 1; j > 1 && !sorted; j--) {
 		sorted = 1;
-		for (int i = 0; i < length - 1; i++)
+		for (int i = 0; i < j; i++)
 			if (array[i] > array[i + 1]) {
 				swap(&array[i], &array[i + 1]);
 				sorted = 0;
