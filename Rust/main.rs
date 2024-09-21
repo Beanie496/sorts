c920c73429a593c7b070756bbb5b3362144ea6fb
Jasper Shovelton <Beanie.github@proton.me>
2024-09-22 00:54:19 +0100

    Add a counting sort.
    I actually added this back in April but never committed it.

--- Rust/main.rs
+++ Rust/main.rs
@@ -1,11 +1,19 @@
+use sorts::{
+    counting_sort,
+    quicksorts::{quicksort_ptr, quicksort_slice},
+};
+
 mod sorts;
 
 fn main() {
     const LEN: usize = 10_000_000;
 
     let mut arr = sorts::make_array(LEN);
-    sorts::quicksorts::quicksort_slice(&mut arr);
+    quicksort_slice(&mut arr);
 
     let mut arr = sorts::make_array(LEN);
-    sorts::quicksorts::quicksort_ptr(&mut arr);
+    quicksort_ptr(&mut arr);
+
+    let mut arr = sorts::make_array(LEN);
+    counting_sort(&mut arr);
 }
--- Rust/sorts.rs
+++ Rust/sorts.rs
@@ -1,7 +1,36 @@
-use std::{arch::x86_64, iter};
+use std::{arch::x86_64, convert::TryInto, iter};
 
-pub mod counting_sort;
 pub mod quicksorts;
+
+pub fn counting_sort(array: &mut [u32]) {
+    let mut secondary_array = vec![0; array.len()];
+    let Some(max): Option<usize> = array.iter().max().and_then(|&m| m.try_into().ok()) else {
+        return;
+    };
+    // '+ 1' because it's 0 to max, not 1
+    let mut counting_list: Vec<usize> = vec![0; max + 1];
+
+    // count occurances of each number
+    for item in array.iter() {
+        counting_list[*item as usize] += 1;
+    }
+
+    // make each entry the cumulative sum of all the previous entries
+    for index in 1..=max {
+        counting_list[index] += counting_list[index - 1];
+    }
+
+    for item in array.iter() {
+        let index = &mut counting_list[*item as usize];
+        // this now refers to the last index of this number instead of how many
+        // of this number there are
+        *index -= 1;
+        // insert
+        secondary_array[*index] = *item;
+    }
+
+    array.copy_from_slice(&secondary_array);
+}
 
 pub fn make_array<T: Copy + From<u16>>(len: usize) -> Vec<T> {
     iter::from_fn(|| {
